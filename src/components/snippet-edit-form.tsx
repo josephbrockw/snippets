0d4fd75cbdf9d085ce3e973d061114d28d821ca4
Joe Wilkinson <me@thejoewilkinson.com>
2025-10-01 14:16:10 -0500

    display monaco editor and add callback func

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import type { Snippet } from '@/generated/prisma';
+import Editor from '@monaco-editor/react';
+import { useState } from 'react';
 
 interface SnippetEditFormProps {
     snippet: Snippet;
@@ -8,5 +10,29 @@ interface SnippetEditFormProps {
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-    return <div>Snippet Edit Form | {snippet.title}</div>;
+    const [code, setCode] = useState(snippet.code || '');
+
+    const handleEditorChange = (value: string = "") => {
+        setCode(value);
+    };
+
+    return (
+        <div>
+            <Editor
+                height="40vh"
+                defaultLanguage="typescript"
+                defaultValue={snippet.code || '// Write your code here'}
+                theme="vs-dark"
+                options={{ 
+                    automaticLayout: true,
+                    minimap: { enabled: false },
+                    fontSize: 14,
+                    lineHeight: 22,
+                    padding: { top: 10, bottom: 10 }
+                }}
+                onChange={handleEditorChange}
+            />
+            {/* Add form fields and buttons as needed */}
+        </div>
+    ) 
 }
